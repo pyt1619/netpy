346850da4a31ce1cf6bf9b297988d817408fd9f0
pyt1619 <54702594+pyt1619@users.noreply.github.com>
2020-04-07 21:19:26 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+# зависисости : Python3, tkinter, toriptables2, tor
+
+# установка toriptables2
+#sudo apt-get install tor
+#sudo systemctl start tor
+#При желании, вы можете добавить службу Tor в автозагрузку:
+#     sudo systemctl enable tor
+#cd
+#git clone https://github.com/ruped24/toriptables2
+#cd toriptables2/
+#sudo mv toriptables2.py /usr/local/bin/
 
 sudo apt install python3 python3-tk
